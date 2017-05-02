3e781727bee389eb93ddb5141c9f857ed2fe1c46
Shanthi robinson <shanthi.robinson100@gmail.com>
2017-05-01 19:02:14 -0700

    ugh

--- capstone.js
+++ capstone.js
@@ -1,1 +1,5 @@
+var step = 1
+var options = document.getElementById("select")
 
+options.innerHTML = "heyo"
+document.getElementById("select").innerHTML = "heyo" + "<img src="images/reg-dough.png" height="100" width="100"> "  
